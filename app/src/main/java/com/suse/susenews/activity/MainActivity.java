bef1874e06273289b001335a30bb9fff46d615ad
xmc <xmc@163.com>
2019-03-12 19:24:17 +0800

    完成部分主页，正在写个人中心的内容

--- app/src/main/java/com/suse/susenews/activity/MainActivity.java
+++ app/src/main/java/com/suse/susenews/activity/MainActivity.java
@@ -2,14 +2,26 @@ package com.suse.susenews.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 
 import com.suse.susenews.R;
+import com.suse.susenews.fragment.MainFragment;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initFragment();
+    }
+
+    private void initFragment() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.fl_main, new MainFragment());
+        transaction.commit();
     }
 }
--- app/src/main/java/com/suse/susenews/utils/CacheUtils.java
+++ app/src/main/java/com/suse/susenews/utils/CacheUtils.java
@@ -5,14 +5,25 @@ import android.content.SharedPreferences;
 
 
 public class CacheUtils {
+    public static final String READ_ARRAY_ID = "read_array_id";
+    public static  String USER_ACCOUNT = "123456";//在登录后更新这个值
 
     public static boolean getBoolean(Context context, String key) {
-        SharedPreferences sp = context.getSharedPreferences("SUSE", Context.MODE_PRIVATE);
+        SharedPreferences sp = context.getSharedPreferences(USER_ACCOUNT, Context.MODE_PRIVATE);
         return sp.getBoolean(key, false);
     }
 
     public static void putBoolean(Context context, String key, boolean value) {
-        SharedPreferences sp = context.getSharedPreferences("SUSE", Context.MODE_PRIVATE);
+        SharedPreferences sp = context.getSharedPreferences(USER_ACCOUNT, Context.MODE_PRIVATE);
         sp.edit().putBoolean(key, value).apply();
+    }
+    public static String getString(Context context, String key) {
+        SharedPreferences sp = context.getSharedPreferences(USER_ACCOUNT, Context.MODE_PRIVATE);
+        return sp.getString(key, "");
+    }
+
+    public static void putString(Context context, String key, String value) {
+        SharedPreferences sp = context.getSharedPreferences(USER_ACCOUNT, Context.MODE_PRIVATE);
+        sp.edit().putString(key, value).apply();
     }
 }
